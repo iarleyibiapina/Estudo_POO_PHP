cb4f2ea930c52e8e92ee8352c6991f4eb06667c3
iarley <7iarley7@gmail.com>
2024-08-04 00:05:00 -0300

    interface

--- INTERFACE/AvaliaInterface.php
+++ INTERFACE/AvaliaInterface.php
@@ -1,6 +1,12 @@
 <?php
 
+// Uma interface é um contrato
+// Uma interface possui todos os metoso 'abstratos'
+// Uma interface sempre deve possui metodos publics
+// Uma interface não define atributos, apenas metodos
 interface AvaliaInterface
 {
     public function avalia();
 }
+
+// Existe um conceito onde: é preciso programar para interfaces, ou depender delas.
